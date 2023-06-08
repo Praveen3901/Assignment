f788ab492b3ca3b0f4907fb508b74a6e5b7a19cb
Praveen3901 <135827343+Praveen3901@users.noreply.github.com>
2023-06-08 13:34:45 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CardListing from './Components/CardListening'
+
+ const App = () => {
+  return (
+    <div>
+      <CardListing/>
+    </div>
+  )
+}
+export default App
+
+
+
+
+
+
